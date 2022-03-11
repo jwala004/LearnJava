117b9cab85d59c442b1e65e611c74ee138bab1b9
Jwala Kumar <41124301+jwala004@users.noreply.github.com>
2022-03-12 00:26:16 +0530

    commit new programs

--- src/main/java/com/intrvw/arrayquestions/DuplicateStringElements.java
+++ src/main/java/com/intrvw/arrayquestions/DuplicateStringElements.java
@@ -3,32 +3,49 @@ package com.intrvw.arrayquestions;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Set;
 
 public class DuplicateStringElements {
 
+	static Integer count;
+
 	public static void main(String[] args) {
 
-		String[] student = { "jwala", "sonu", "monu", "sonu", "anu", "anu", "anu", "sonu", "anuu" };
+		String[] student = { "jwala", "sonu", "monu", "sonu", "anu", "anu", "anu", "son", "anuu" };
 
 		System.out.println("Duplicate elements from array using hash table");
 
-		Map<String, Integer> nameAndCount = new HashMap<>();
+		Map<String, Integer> map = new HashMap<>();
 		// build hash table with count
 
 		for (String name : student) {
-			Integer count = nameAndCount.get(name);
+//			int counter = 0;
+			count = map.get(name);
+			System.out.println("Count value - " + count);
 			if (count == null) {
-				nameAndCount.put(name, 1);
+				map.put(name, 1);
+
+				System.out.println("Count value -- " + count);
+
 			} else {
-				nameAndCount.put(name, ++count);
+				map.put(name, ++count);
+				System.out.println("Count value --- " + count);
+//				counter++;
+			}
+
+			if (Objects.nonNull(count)) {
+				System.out.println("Count value ---- " + count);
+				System.out.println("Duplicate element from array : " + " occurance count " + count);
 			}
 		}
 		// Print duplicate elements from array in Java
-		Set<Entry<String, Integer>> entrySet = nameAndCount.entrySet();
+		Set<Entry<String, Integer>> entrySet = map.entrySet();
 		for (Entry<String, Integer> entry : entrySet) {
 			if (entry.getValue() > 1) {
-				System.out.println("Duplicate element from array : " + entry.getKey());
+//				System.out.println("Duplicate element from array : " + entry.getKey() + " occurance count ");
+
+				System.out.println("Count value ------** " + count);
 
 			}
 		}
