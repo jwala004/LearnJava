0b9bbfc7e7b0bd1d20d82ce56722ebc9b0275147
Jwala Kumar <41124301+jwala004@users.noreply.github.com>
2022-03-29 00:18:57 +0530

    commit modified change

--- src/main/java/java8/naveenautomationlabs/interviewquestions/DuplicateStringElements.java
+++ src/main/java/java8/naveenautomationlabs/interviewquestions/DuplicateStringElements.java
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 package java8.naveenautomationlabs.interviewquestions;
 
 import java.util.HashMap;
-=======
-package java8.naveenautomationlabs.javaprogram.interviewquestions;
 
 import java.util.HashMap;
-import java.util.HashSet;
->>>>>>> 323e7fb8857b3e95249a2bd71f6561e434cdf684
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -20,25 +15,22 @@ public class DuplicateStringElements {
 				"Ruby", "C" };
 
 //		M1: using Brute-force
-<<<<<<< HEAD
 
 //		for (int i = 0; i < arr.length; i++) {
 //
 //			for (int j = i+1; j < arr.length; j++) {
-=======
 //		for (int i = 0; i < arr.length; i++) {
 //
 //			for (int j = i + 1; j < arr.length; j++) {
->>>>>>> 323e7fb8857b3e95249a2bd71f6561e434cdf684
 //
 //				if (arr[i] == arr[j]) {
 //					System.out.println(arr[i]);
 //				}
-<<<<<<< HEAD
+
 //				break; // using break will print the duplicate values only once, otherwise it will be printed based on occurance
 //			}
 //		}
-		
+
 //	Brute-force has	time complexity of O(n^2) 
 
 //		M2: using HashSet 
@@ -74,43 +66,6 @@ public class DuplicateStringElements {
 //				System.out.println(entr.getKey()+" "+entr.getValue());
 			}
 
-=======
-//				break; // using break will print the duplicate values only once, otherwise it will be
-//						// printed based on occurance
-//			}
-//		}
-
-//		M2: using HashSet 
-//		Set<String> set = new HashSet<String>();
-//
-//		for (int i = 0; i < arr.length; i++) {
-//
-//			if (set.add(arr[i]) == false) {
-//				System.out.println(arr[i]);
-//			}
-//		}
-
-//		M2: using HashMap
-		Map<String, Integer> map = new HashMap<String, Integer>();
-
-		for (String str : arr) {
-			Integer count = map.get(str);
-			if (count == null) {
-				map.put(str, 1);
-			} else {
-				map.put(str, ++count);
-			}
-		}
-
-//		Now, Print all the duplicate elements
-		Set<Entry<String, Integer>> entrySet = map.entrySet();
-
-		for (Entry<String, Integer> entry : entrySet) {
-			if (entry.getValue() > 1) {
-				System.out.println(entry.getKey());
-//				System.out.println(entry.getKey() +" "+entry.getValue());
-			}
->>>>>>> 323e7fb8857b3e95249a2bd71f6561e434cdf684
 		}
 
 	}
