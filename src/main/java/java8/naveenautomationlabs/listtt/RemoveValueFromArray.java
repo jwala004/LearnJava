14196ccaa8f829318d3081caa7a4d249b7a9165c
Jwala Kumar <41124301+jwala004@users.noreply.github.com>
2022-04-11 10:51:33 +0530

    commit new interview quesn office

---
+++ src/main/java/java8/naveenautomationlabs/listtt/RemoveValueFromArray.java
@@ -0,0 +1,22 @@
+package java8.naveenautomationlabs.listtt;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class RemoveValueFromArray {
+
+	public static void main(String[] args) {
+
+		int[] arry = { 1, 2, 3, 4, 5, 55, 12, 35 };
+
+		List<Integer> list = new ArrayList(Arrays.asList(arry));
+
+		Iterator it = list.iterator();
+		
+	
+
+	}
+
+}
