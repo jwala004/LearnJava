afa44e8fbacc5e267e52b21e0c841cc53fe8e8d1
Jwala Kumar <41124301+jwala004@users.noreply.github.com>
2024-02-08 23:48:15 +0530

    commit changes

---
+++ src/company/AirtelIntervw.java
@@ -0,0 +1,30 @@
+package company;
+
+public class AirtelIntervw {
+
+//	Input: Jwala Loves Programming
+//	Output: awalJ  soveL grogramminP
+
+	public static void main(String[] args) {
+
+		String str = "Jwala Loves Programming";
+
+		String newStr = null, toReverse = "";
+
+		String[] arr = str.split(" ");
+
+		for (int i = 0; i < arr.length; i++) {
+
+			newStr = i + "";
+
+			for (int j = newStr.length() - 1; j >= 0 ; j--) {
+				
+				toReverse = toReverse + newStr.charAt(j);
+				
+			}
+
+		}
+
+	}
+
+}
